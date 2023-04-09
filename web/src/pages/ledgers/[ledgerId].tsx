4f7e22c1e41d62bd3b2b4865088d6703795f01f8
ethan-stone <ethan.stone9352@gmail.com>
2023-04-09 00:28:19 -0400

    add basic ledger page with button to make new entry

--- web/src/pages/ledgers/[ledgerId].tsx
+++ web/src/pages/ledgers/[ledgerId].tsx
@@ -1,3 +1,4 @@
+import Spinner from "@/components/spinner";
 import { api } from "@/utils/api";
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
@@ -6,16 +7,34 @@ const Ledger: NextPage = () => {
   const router = useRouter();
   const { ledgerId } = router.query as { ledgerId: string | undefined };
 
-  const { data } = api.ledgers.getLedger.useQuery(
-    {
-      ledgerId: ledgerId as string,
-    },
-    {
-      enabled: ledgerId !== undefined,
-    }
-  );
+  const { data: ledger, isLoading: isLedgerLoading } =
+    api.ledgers.getLedger.useQuery(
+      {
+        ledgerId: ledgerId as string,
+      },
+      {
+        enabled: ledgerId !== undefined,
+      }
+    );
 
-  return <div>Hi</div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-neutral-200">
+      <div className="container flex flex-grow flex-col items-center justify-center">
+        {isLedgerLoading ? (
+          <Spinner />
+        ) : !ledger ? (
+          <div>not found</div>
+        ) : (
+          <div className="flex flex-grow flex-col items-center justify-center">
+            <p>{ledger.name}</p>
+            <button className="mt-4 rounded border border-neutral-900 p-2">
+              New Entry
+            </button>
+          </div>
+        )}
+      </div>
+    </main>
+  );
 };
 
 export default Ledger;
--- web/src/pages/ledgers/index.tsx
+++ web/src/pages/ledgers/index.tsx
@@ -93,6 +93,7 @@ const Ledgers: NextPage = () => {
                       </Link>
                     );
                   }
+
                   return (
                     <Link
                       key={ledger.id}
